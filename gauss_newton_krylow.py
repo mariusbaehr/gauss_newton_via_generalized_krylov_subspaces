31ddbab1f2da38ba85b94690bc7efaba94eed17a
mariusbaehr <mariusbaehr@users.noreply.github.com>
2025-09-29 12:20:21 +0200

    refactor work in progress: now only use GeneralizedKrylowSubspace class to store and update basis as well as evaluations of functions on subspace. Jac will now be evaluated explicitly in gauss_newton_krylow. GeneralizedKrylowSubspaceBreakdown error added.

--- gauss_newton_krylow.py
+++ gauss_newton_krylow.py
@@ -8,125 +8,59 @@ import numpy.typing as npt
 import scipy.sparse as sp
 import scipy
 
+class GeneralizedKrylowSubspaceBreakdown(Exception):
+    pass # TODO: Not shure if Error is apropriate, as it could naturaly happen
+        # TODO: Maybe add error message here
 
-class Krylow:
+
+
+
+class GeneralizedKrylowSubspace:
     """
     The goal of this class is to outsource all the additional complexity from gauss_newton_krylow. To be close to the implementation of gauss_newton.
 
     Attributes
     ----------
-    base:
-    active_columns:
-    _res:
-    _jac:
-    krylow_max_dim:
-    res_ev:
-    restart:
+    basis:
     """
 
-    base: npt.NDArray
-    active_columns: int
-    _res: Callable[[npt.NDArray, Tuple[Any]], npt.NDArray]
-    _jac: Callable[
-        [npt.NDArray, Tuple[Any]], Union[npt.NDArray, sp.spmatrix]
-    ]
-    krylow_max_dim: int
-    res_ev: npt.NDArray
-    restart: bool
+    basis: npt.NDArray
 
-    def __init__(
-        self,
-        _res: Callable[[npt.NDArray, Tuple[Any]], npt.NDArray],
-        x0: npt.NDArray,
-        _jac: Callable[
-            [npt.NDArray, Tuple[Any]], Tuple[npt.NDArray, sp.spmatrix]
-        ],
-        krylow_max_dim: int,
-        restart: bool,
-        *args
-    ):
+    def __init__(self):
+        pass
 
+
+    def start(self, x0: npt.NDArray) -> npt.NDArray:
         if np.allclose(x0, np.zeros_like(x0)):
             raise ValueError(
                 "x0 is not allowed to be 0 in the gauss_newton_krylow algorithm"
             )
 
-        self._res = _res
-        self._jac = _jac
-        self.active_columns = 1
-        self.krylow_max_dim = krylow_max_dim
-        self.restart = restart
+        x0_norm = np.linalg.norm(x0)
+        self.basis = (x0 / x0_norm).reshape(-1,1)
+        x_coordinate = np.array([x0_norm])
+        return x_coordinate
 
-        self.base = np.zeros((len(x0), self.krylow_max_dim))
-        self.base[:, 0] = x0 / np.linalg.norm(x0)
+    def x(self, x_coordinate: npt.NDArray)->npt.NDArray:
+        return self.basis@ x_coordinate
 
-        self.jac_ev = self._jac(
-            self.base[:, : self.active_columns] @ np.array([np.linalg.norm(x0)]), *args
-        )
-
-    def x(self, x_coordinate: npt.NDArray):
-        return self.base[:, : self.active_columns] @ x_coordinate
-
-    def res(self, x_coordinate: npt.NDArray, *args: Any) -> npt.NDArray:
-        return self._res(self.base[:, : self.active_columns] @ x_coordinate, *args)
-
-    # def jac(self, x_coordinate: npt.NDArray, *args:Any) -> npt.NDArray:
-    #    jac_ev = self._jac(self.base[:,:self.active_columns]@x_coordinate, *args)
-    #    self.normal_res = jac_ev.T@self.res_ev
-    #    return jac_ev@self.base[:,:self.active_columns]
-
-    def jac(self) -> npt.NDArray:
-        # Die Krylow auswertung wird so versteckt !!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!
-        return self.jac_ev @ self.base[:, : self.active_columns]
+    def evaluate(self, fun, x_coordinate: npt.NDArray,*args: Any):
+        return fun(self.x(x_coordinate),*args)
 
     def update(
-        self, x_coordinate: npt.NDArray, res_ev: npt.NDArray, *args: Tuple[Any]
-    ) -> npt.NDArray:
-        """
-        Because the dimensions may change x_coordinate is also updatet in size.
-        """
-
-        #np.testing.assert_allclose(self.base[:,:self.active_columns].T @ self.base[:,:self.active_columns], np.eye(self.active_columns), atol=1E-10, rtol=0, err_msg=f"first violation = {self.active_columns}")
-
-        self.jac_ev = self._jac(
-            self.base[:, : self.active_columns] @ x_coordinate, *args
-        )
-
-        #print(self.base[:,:self.active_columns].T @ self.base[:,:self.active_columns])
-
-        normal_res = self.jac_ev.T @ res_ev
-        if self.active_columns == self.krylow_max_dim and not self.restart:
-            print(
-                "Warning: gauss_newton_krylow algorithm reached krylow_max_dim. Further iterations are equivalent to the standart gauss_newton algorithm but much slower."
-            )
-
-            # np.testing.assert_allclose(self.base.T @ self.base, np.eye(len(self.base)))
-
-            return x_coordinate
-
-
-        normal_res -= self.base[:, : self.active_columns] @ (
-            self.base[:, : self.active_columns].T @ normal_res
-        )
+        self, jac_ev: Union[npt.NDArray,sp.spmatrix], res_ev: npt.NDArray
+    ) -> None:
+        normal_res = jac_ev.T @ res_ev #TODO: Check sign
+        normal_res -= self.basis @ ( self.basis.T @ normal_res)
 
         if np.allclose(normal_res, np.zeros_like(normal_res)):
-            return x_coordinate
+            raise GeneralizedKrylowSubspaceBreakdown(
+                "Normal residual is allready inside generalized Krylow Subspcae, there for gauss newton krylow algorithm has to proceed without enlarging subspace."
+            )
 
         normal_res /= np.linalg.norm(normal_res)
-
-        if self.active_columns == self.krylow_max_dim and self.restart:
-            x0 = self.x(x_coordinate)
-            x_coordinate = np.array([np.linalg.norm(x0)])
-            self.base = np.zeros((len(normal_res), self.krylow_max_dim))
-            self.base[:, 0] = x0 / x_coordinate[0]
-            self.active_columns = 1
-        else:
-
-            self.base[:, self.active_columns] = normal_res
-            self.active_columns += 1
-            x_coordinate = np.block([x_coordinate, np.zeros(1)])
-
-        return x_coordinate
+        normal_res = normal_res.reshape(-1,1)
+        self.basis = np.hstack([self.basis, normal_res])
 
 
 def gauss_newton_krylow(
@@ -147,7 +81,7 @@ def gauss_newton_krylow(
     res: The residual function, called as res(x, *args) the argument x and its return must always be ndarrays.
     x0: Initial guess of the regression parameters.
     jac: 
-    krylow_restart: If the krylow_base gets larger than krylow_restart the base is reset, if left to None base never resets.
+    krylow_restart: If the krylow_basis gets larger than krylow_restart the basis is reset, if left to None basis never resets.
     args: Additional arguments passed to res and jac.
     tol: Tolerance for termination by the change of the paramters x.
     max_iter: Maximum number of iterations.
@@ -160,31 +94,29 @@ def gauss_newton_krylow(
 
     success: bool = False
 
-    restart: bool = True
-    if krylow_restart == None:
-        krylow_restart = max_iter
-        restart = False
+    krylow = GeneralizedKrylowSubspace()
+    x_coordinate = krylow.start(x0)
 
-    krylow_max_dim: int = min(max_iter, krylow_restart, len(x0))
-    x_coordinate = np.array([np.linalg.norm(x0)])
+    def res_krylow(x_coordinate,*args):
+        return krylow.evaluate(res,x_coordinate,*args)
 
-    krylow = Krylow(res, x0.copy(), jac, krylow_max_dim, restart, *args)
-
-    res_ev: npt.NDArray = krylow.res(x_coordinate, *args)
+    res_ev_new: npt.NDArray = res_krylow(x_coordinate,*args)
     nfev: int = 1
+    jac_ev: sp.spmatrix = jac(x0,*args)
+    njev: int = 1 #TODO: Use this instead of iter
 
     for iter in range(max_iter):
 
-        jac_ev: npt.NDArray = krylow.jac()
+        jac_krylow = jac_ev @ krylow.basis
+        res_ev = res_ev_new
 
         descent_direction, _, _, _ = scipy.linalg.lstsq(
             -1 * jac_ev, res_ev 
         )
 
-        step_length, res_ev, nfev_delta = armijo_goldstein(
-            krylow.res, x_coordinate, res_ev, jac_ev, args, descent_direction
+        step_length, res_ev_new, nfev_delta = armijo_goldstein(
+            res_krylow, x_coordinate, res_ev, jac_krylow, args, descent_direction
         )
-        print(step_length)
         nfev += nfev_delta
 
         squared_sum_x_prev = np.sum(x_coordinate**2)
@@ -206,7 +138,15 @@ def gauss_newton_krylow(
             success = True
             break
 
-        x_coordinate = krylow.update(x_coordinate, res_ev, *args)
+        jac_ev = krylow.evaluate(jac, x_coordinate)
+
+        try: 
+            krylow.update(jac_ev.T,res_ev)
+            x_coordinate = np.append(x_coordinate,0)
+        except GeneralizedKrylowSubspaceBreakdown:
+            pass
+
+
 
     if not success:
         print(
