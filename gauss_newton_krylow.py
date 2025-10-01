fix: test was bugged, vectors still slowly become numerical linearily dependend

gauss_newton_krylow.py
@@ -64,7 +64,7 @@ class GeneralizedKrylowSubspace:
         self, jac_ev: Union[npt.NDArray,sp.spmatrix], res_ev: npt.NDArray
     ) -> None:
         ATA=self.basis.T @ self.basis
-        ATA-= np.ones_like(ATA)
+        ATA-= np.eye(ATA.shape[0])
         print(np.linalg.norm(ATA))
         normal_res = jac_ev.T @ res_ev #TODO: Check sign
         #normal_res -= self.basis @ ( self.basis.T @ normal_res)