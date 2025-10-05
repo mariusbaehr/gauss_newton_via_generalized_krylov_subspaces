2cea50c37f4e11b8fcaded1ef675023a499867ef
mariusbaehr <mariusbaehr@users.noreply.github.com>
2025-10-05 14:12:10 +0200

    refactor: removed secant.py because it makes no sense to further modify gauss_newton_krylow if the original alogrithm dosn't work well

--- rosenbrock_test.py
+++ rosenbrock_test.py
@@ -2,7 +2,6 @@ import numpy as np
 import scipy.sparse
 from benchmark import benchmark
 
-
 p = 100  # N=2p-2, again not a classical regression model, however
 
 
@@ -11,9 +10,6 @@ def res(x):
     block2 = 1 - x[:-1]
     return np.concatenate([block1, block2])
 
-
-
-
 def jac(x):
     block1 = 10 * scipy.sparse.eye(p - 1, p, k=1) - 20 * scipy.sparse.diags(
         x[:-1], shape=(p - 1, p)
@@ -21,9 +17,7 @@ def jac(x):
     block2 = -scipy.sparse.eye(p - 1, p, k=0)
     return scipy.sparse.block_array([[block1], [block2]])
 
-
 x_exact = np.ones(p)
-
 
 def error(x):
     return np.linalg.norm(x - x_exact)
@@ -33,7 +27,7 @@ from gauss_newton import gauss_newton
 if __name__ == '__main__':
 
     x0 = np.zeros(p)
-    x0[0] = 1.5  # Because zero is not allowed in gauss_newton_krylow
+    x0[0] = 1.5 # Because zero is not allowed in gauss_newton_krylow
     benchmark(res, x0, jac, error, {"max_iter": 500}, title="rosenbrock_i_")
     
     x0_ii = gauss_newton(res, x0, jac,max_iter=155).x
