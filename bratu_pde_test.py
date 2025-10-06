d0ba732cf1a2d361d89437221c3f399cfa2e46e5
mariusbaehr <mariusbaehr@users.noreply.github.com>
2025-10-06 14:58:57 +0200

    feat: tried new x0

--- bratu_pde_test.py
+++ bratu_pde_test.py
@@ -42,17 +42,15 @@ def y_true(h):
 # y_true = lamb_f(x1, x2).flatten()
 
 
-# x0 = np.zeros_like(x_true)
-# x0[1] = 1
-# x0= A_h2.T@y_true+a1*D_x1.T@y_true
-x0 = x_true + 10**-1 * np.ones_like(x_true)
-
-
-# x0 /= np.linalg.norm(x0)
+#x0 = np.zeros_like(x_true)
+#x0[1] = 1
+#x0= A_h2.T@y_true+a1*D_x1.T@y_true
+x0 = -2*x_true #+ 10**-1 * np.ones_like(x_true)
 
 
 def res(x,h):
     return y_true(h) - h**-2 * A2 @ x - h**-1 * a1 * Dx1 @ x - a2 * np.exp(x)
+
 
 def jac(x,h):
     return - h**2 * A2 - h**-1 * a1 * Dx1 - a2 * scipy.sparse.diags(np.exp(x))
@@ -71,15 +69,19 @@ ATAh = scipy.sparse.linalg.LinearOperator(((n - 1) ** 2, (n - 1) ** 2), matvec= 
 
  #def ref_cg(callback):
  #    cg, _ = scipy.sparse.linalg.cg(
- #        A_TA, A_h2.T @ y_true + a1 * D_x1.T @ y_true, x0, callback=callback
+ #        ATA, A2.T @ y_true + a1 * Dx1.T @ y_true, x0, callback=callback
  #    )
  #    return cg
 
 
 if __name__ == "__main__":
     benchmark(
-        res, x0, jac, error, {"args": (1,), "max_iter": 100, "tol": 1e-12}, title="bratu_pde"
+        res, x0, jac, error, {"args": (h,), "max_iter": 100, "tol": 1e-12}, title="bratu_pde"
     )  # ,additional_methods=[ref_cg])
+
+    benchmark(
+        res, x0, jac, error, {"args": (1,), "max_iter": 100, "tol": 1e-12}, title="bratue_pde_h"
+    )
 
 # print(f"condition number of A.T@A {np.linalg.cond( ((A_h2+a1*D_x1).T@(A_h2+a1*D_x1)).todense())}")
 # print(f"norm of A.T@A {np.linalg.norm(((A_h2+a1*D_x1).T@(A_h2+a1*D_x1)).todense(),2)}")
