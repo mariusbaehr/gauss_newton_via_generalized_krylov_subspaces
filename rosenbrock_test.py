5bd4302b75e61fa6bc059ad1d747088480d6a2f8
mariusbaehr <mariusbaehr@users.noreply.github.com>
2025-10-23 14:26:06 +0200

    refactor: changed benchmark, now plot manualy

--- rosenbrock_test.py
+++ rosenbrock_test.py
@@ -1,6 +1,10 @@
 import numpy as np
 import scipy.sparse
+import matplotlib.pyplot as plt
 from benchmark import benchmark
+from newbenchmark import benchmark_method, ref_method
+from gauss_newton import gauss_newton
+from gauss_newton_krylow import gauss_newton_krylow
 
 p = 100  # N=2p-2, again not a classical regression model, however
 
@@ -25,21 +29,100 @@ x_exact = np.ones(p)
 def error(x):
     return np.linalg.norm(x - x_exact)
 
+
+def compare_error_i():
+    np.random.seed(42)
+    x0 = x_exact + 0.1 * np.random.normal(loc=0, scale=1, size=p)
+
+    gn_data = benchmark_method(gauss_newton, res, x0, jac, error)
+    gnk_data = benchmark_method(gauss_newton_krylow, res, x0, jac, error)
+    gnk_ii_data = benchmark_method(gauss_newton_krylow, res, x0, jac, error, kwargs = {"version":"res_new"})
+    ref_data = benchmark_method(ref_method, res, x0, jac, error)
+
+    fig1, ax1 = plt.subplots(figsize=(8, 4), dpi=300)
+    ax1.set_xlabel("Iterationen")
+    ax1.set_ylabel(r"Fehler $\log\|x_k-x^\ast\|$")
+    fig2, ax2 = plt.subplots(figsize=(8, 4), dpi=300)
+    ax2.set_xlabel("Iterationen")
+    ax2.set_ylabel(r"Verlust $\log\mathcal{L}$")
+    fig3, ax3 = plt.subplots(figsize=(8, 4), dpi=300)
+    ax3.set_xlabel("Iterationen")
+    ax3.set_ylabel(r"Anzahl $f$ Auswertungen")
+    fig4, ax4 = plt.subplots(figsize=(8, 4), dpi=300)
+    ax4.set_xlabel("Iterationen")
+    ax4.set_ylabel(r"Anzahl CG-Iterationen")
+
+
+    ax1.semilogy(gn_data[0],"-x",label="gn")
+    ax1.semilogy(gnk_data[0],"-x",label="gnk")
+    ax1.semilogy(gnk_ii_data[0],"-x",label="gnk_ii")
+    ax1.semilogy(ref_data[0],"-x",label="ref")
+
+
+    ax2.semilogy(gn_data[1],"-x",label="gn")
+    ax2.semilogy(gnk_data[1],"-x",label="gnk")
+    ax2.semilogy(gnk_ii_data[1],"-x",label="gnk_ii")
+    ax2.semilogy(ref_data[1],"-x",label="ref")
+
+
+    ax3.plot(gn_data[2],"-x",label="gn")
+    ax3.plot(gnk_data[2],"-x",label="gnk")
+    ax3.plot(gnk_ii_data[2],"-x",label="gnk_ii")
+    ax3.plot(ref_data[2],"-x",label="ref")
+
+    ax4.plot(gn_data[3],"-x",label="gn")
+
+
+    ax1.legend()
+    ax2.legend()
+    ax3.legend()
+    ax4.legend()
+    plt.show()
+
+
+def compare_error_ii():
+    x0 = 2 * x_exact
+
+    gn_data = benchmark_method(gauss_newton, res, x0, jac, error)
+    gnk_data = benchmark_method(gauss_newton_krylow, res, x0, jac, error)
+    gnk_ii_data = benchmark_method(gauss_newton_krylow, res, x0, jac, error, kwargs = {"version":"res_new"})
+    ref_data = benchmark_method(ref_method, res, x0, jac, error)
+
+    plt.figure(figsize=(8,4), dpi =300)
+    plt.semilogy(gn_data[0],"-x",label="gn")
+    plt.semilogy(gnk_data[0],"-x",label="gnk")
+    plt.semilogy(gnk_ii_data[0],"-x",label="gnk_ii")
+    plt.semilogy(ref_data[0],"-x",label="ref")
+    plt.xlabel("Iterationen")
+    plt.ylabel(r"Fehler $\log\|x_k-x^\ast\|$")
+    plt.legend()
+    plt.show()
+
+def compare_error_iii():
+
+    x0 = 2 * x_exact
+    x0[0] = 1
+
+    gn_data = benchmark_method(gauss_newton, res, x0, jac, error)
+    gnk_data = benchmark_method(gauss_newton_krylow, res, x0, jac, error)
+    gnk_ii_data = benchmark_method(gauss_newton_krylow, res, x0, jac, error, kwargs = {"version":"res_new"})
+    ref_data = benchmark_method(ref_method, res, x0, jac, error)
+
+    plt.figure(figsize=(8,4), dpi =300)
+    plt.semilogy(gn_data[0],"-x",label="gn")
+    plt.semilogy(gnk_data[0],"-x",label="gnk")
+    plt.semilogy(gnk_ii_data[0],"-x",label="gnk_ii")
+    plt.semilogy(ref_data[0],"-x",label="ref")
+    plt.xlabel("Iterationen")
+    plt.ylabel(r"Fehler $\log\|x_k-x^\ast\|$")
+    plt.legend()
+    plt.show()
+
+
 if __name__ == "__main__":
 
-#    x0 = np.zeros(p)
-#    x0[0] = -0.5  # Because zero is not allowed in gauss_newton_krylow
-#    benchmark(res, x0, jac, error, {"max_iter": 500}, title="rosenbrock_i_")
+    compare_error_i()
+    compare_error_ii()
+    compare_error_iii()
 
-    np.random.seed(42)
-    x0_i = x_exact + 0.1 * np.random.normal(loc=0, scale=1, size=p)
-    print(x0_i)
-    benchmark(res, x0_i, jac, error, {"max_iter": 500}, title="rosenbrock_i_")
 
-    x0_ii = 2 * x_exact
-    benchmark(res, x0_ii, jac, error, {"max_iter": 500}, title="rosenbrock_ii_")
-
-    x0_iii = x0_ii.copy()
-    x0_iii[0] = 1
-
-    benchmark(res, x0_iii, jac, error, {"max_iter": 500}, title="rosenbrock_iii_")
