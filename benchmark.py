32f607655614c54c547af41e06123c63dc2fc31f
mariusbaehr <mariusbaehr@users.noreply.github.com>
2025-10-24 12:55:39 +0200

    refactor: completly changed benchmark.py now it dosnt plot anymore

--- benchmark.py
+++ benchmark.py
@@ -1,173 +1,27 @@
-import timeit
-import scipy.optimize
-import matplotlib.pyplot as plt
 import numpy as np
-from gauss_newton import gauss_newton
-from gauss_newton_krylow import gauss_newton_krylow
+import scipy.optimize
 
-plt.rcParams.update(
-    {
-        "text.usetex": True,
-        "font.family": "serif",
-        "font.serif": ["Computer Modern"],
-        "pgf.rcfonts": False,
-    }
-)
+def ref_method(res,x0, jac, args, callback, **kwargs):
+    def cb_scipy(intermediate_result):
+        callback(intermediate_result.x,intermediate_result.nfev,None)
+    scipy.optimize.least_squares(res, x0, jac, callback=cb_scipy, args=args)
 
+def benchmark_method(method,res,x0,jac,error,args=(),kwargs={}):
 
-def benchmark(res, x0, jac, error, kwargs={}, additional_methods=[], title=None):
-    """
-    For fast but still somewhat flexible benchmarks
+    def loss(x):
+        return 0.5*np.sum(res(x,*args)**2)
 
-    Parameters
-    ----------
-    res:
-    x0:
-    jac:
-    error:
-    args: Additional keyword arguments for gauss_newton
-
-
-    """
-    # TODO: make error optional as it is not always available
-
-    if "args" in kwargs:
-
-        def loss(x):
-            return 1/2*np.sum(res(x, *kwargs["args"]) ** 2)
-
-    else:
-
-        def loss(x):
-            return 1/2*np.sum(res(x) ** 2)
-
-    def callback(x, nfev):
-        global error_list, loss_list, nfev_list
-        error_list.append(error(x))
-        loss_list.append(loss(x))
-        nfev_list.append(nfev)
-
-    def callback_gn(x, nfev, cg_iter):
-        global error_list, loss_list, nfev_list, cg_iter_list
+    error_list = [error(x0)]
+    loss_list = [loss(x0)]
+    nfev_list = []
+    cg_iter_list = []
+    def callback(x, nfev, cg_iter):
         error_list.append(error(x))
         loss_list.append(loss(x))
         nfev_list.append(nfev)
         cg_iter_list.append(cg_iter)
 
-    def callback_scipy(intermediate_result):
-        global error_list, loss_list, nfev_list
-        error_list.append(error(intermediate_result.x))
-        loss_list.append(loss(intermediate_result.x))
-        nfev_list.append(intermediate_result.nfev)
-        # print(intermediate_result.njev)
 
-    def callback_cg(x):
-        global error_list, loss_list, nfev_list
-        error_list.append(error(x))
-        loss_list.append(loss(x))
-        nfev_list.append(0)
+    method(res,x0,jac,args=args, callback=callback, **kwargs)
 
-    def gn(callback):
-        return gauss_newton(res, x0, jac, callback=callback, **kwargs)
-
-    if "args" in kwargs:
-
-        def ref_method(callback):
-            return scipy.optimize.least_squares(
-                res, x0, jac, callback=callback, args=kwargs["args"]
-            )
-
-    else:
-
-        def ref_method(callback):
-            return scipy.optimize.least_squares(res, x0, jac, callback=callback)
-
-    def gnk(callback):
-        return gauss_newton_krylow(res, x0, jac, callback=callback, **kwargs)
-
-    def gnk_ii(callback):
-        return gauss_newton_krylow(
-            res, x0, jac, callback=callback, version="res_new", **kwargs
-        )
-
-    def gnk_iii(callback): return gauss_newton_krylow( res, x0, jac, callback=callback, version="jac_old_res_old", **kwargs
-        )
-
-    def gnk_iv(callback):
-        return gauss_newton_krylow(
-            res, x0, jac, callback=callback, version="jac_old_res_new", **kwargs
-        )
-
-    #def gnk_ii_restart(callback): return gauss_newton_krylow( res, x0, jac, callback=callback, krylow_restart=20, version="res_new", **kwargs,)
-
-    methods = [
-        ref_method,
-        gnk,
-        #gnk_ii_restart,
-        gn,
-        gnk_ii,
-        #gnk_iii,
-        #gnk_iv,
-    ] + additional_methods
-    # methods = [ref_method, gnk, gn, gnk_new_res] + additional_methods
-
-    fig1, ax1 = plt.subplots(figsize=(8, 4), dpi=300)
-    ax1.set_xlabel("Iterationen")
-    ax1.set_ylabel(r"Fehler $\log\|x_k-x^\ast\|$")
-    fig2, ax2 = plt.subplots(figsize=(8, 4), dpi=300)
-    ax2.set_xlabel("Iterationen")
-    ax2.set_ylabel(r"Verlust $\log\mathcal{L}$")
-    fig3, ax3 = plt.subplots(figsize=(8, 4), dpi=300)
-    ax3.set_xlabel("Iterationen")
-    ax3.set_ylabel(r"Anzahl $f$ Auswertungen")
-    fig4, ax4 = plt.subplots(figsize=(8, 4), dpi=300)
-    ax4.set_xlabel("Iterationen")
-    ax4.set_ylabel(r"Anzahl CG-Iterationen")
-    #    ax1.set_title("Error Plot")
-    #    ax2.set_title("Loss Plot")
-    #    ax3.set_title("fnev Plot")
-    timeit_number = 1
-    for method in methods:
-        global error_list, loss_list, nfev_list, cg_iter_list
-        error_list = [error(x0)]
-        loss_list = [loss(x0)]
-        nfev_list = [0]
-        cg_iter_list = [0]
-        if method.__name__ == "ref_method":
-            # time = (
-            #    timeit.timeit(lambda: method(None), number=timeit_number)
-            #    / timeit_number
-            # )
-            method(callback_scipy)
-        elif method.__name__ == "ref_cg":
-            method(callback_cg)
-            # time = None
-        elif method.__name__ == "gn":
-            method(callback_gn)
-        else:
-            # time = (
-            # timeit.timeit(lambda: method(lambda: None), number=timeit_number)
-            # / timeit_number
-            # )
-            method(callback)
-
-        # print(f"method = {method.__name__} time = {time} ")
-
-        nit = len(error_list)
-        ax1.semilogy(range(nit), error_list, "x-", label=method.__name__)
-        ax2.semilogy(range(nit), loss_list, "x-", label=method.__name__)
-        ax3.plot(range(nit), nfev_list, "x-", label=method.__name__)
-        ax4.plot(range(len(cg_iter_list)), cg_iter_list, "x-", label=method.__name__)
-
-    ax1.legend()
-    ax2.legend()
-    ax3.legend()
-    ax4.legend()
-
-    if not title == None:
-        fig1.savefig(title + "error" + ".png", bbox_inches="tight")
-        fig2.savefig(title + "loss" + ".png", bbox_inches="tight")
-        fig3.savefig(title + "nfev" + ".png", bbox_inches="tight")
-        fig4.savefig(title + "cg_iter" + ".png", bbox_inches="tight")
-
-    plt.show()
+    return error_list, loss_list, nfev_list, cg_iter_list
